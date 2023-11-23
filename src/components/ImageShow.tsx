94e0447d7c7fc64dba43b75a6c90cd02fe7f68fd
Lucas-Masaba <masabaluke@gmail.com>
2023-11-23 20:46:51 +0300

    Add Props comment

--- src/components/ImageShow.tsx
+++ src/components/ImageShow.tsx
@@ -4,6 +4,14 @@ interface ImageTypeProps {
   image: ImagesType;
 }
 
+// Alternatively, you can add props to  images like this:
+
+// const ImageShow = ({ image }: {image: ImagesType}) => {
+//   return <div>
+//     <img src={image.urls.small} alt={image.alt_description} />
+//     </div>
+// }
+
 const ImageShow = ({ image }: ImageTypeProps) => {
   return <div>
     <img src={image.urls.small} alt={image.alt_description} />
